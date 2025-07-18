231f3299ea59673c08e70de857e282da03e941dc
Sumit Ramteke <sumitvramteke@outlook.com>
2025-07-18 13:46:54 +0800

    removing next export stage and adding in config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false;
 const repo = "drscanprofile"; // your repo name
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: { unoptimized: true },
   basePath: isGithubActions ? `/${repo}` : "",
   assetPrefix: isGithubActions ? `/${repo}/` : "",
